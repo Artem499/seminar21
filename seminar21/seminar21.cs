99a0c3b5d137ce23a4b7fe53d357ce482d3b61d6
Artem <1992.s.a.a@gmail.com>
2022-12-09 09:04:43 +0300

    rename

---
+++ seminar21/seminar21.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+
+Console.WriteLine("Enter a number from 1 to 7:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if(day < 1 || day > 7)
+    Console.WriteLine("Error! Enter a number from 1 to 7");
+    
+if(day == 1 || day == 2 || day == 3 || day == 4 || day == 5) 
+    Console.WriteLine("Result: NO");
+
+if(day == 6 || day == 7)
+    Console.WriteLine("Result: YES");
+
